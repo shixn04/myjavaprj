3c9b2a2385f71b6ac0cb10e1eae357da933c1fd3
shixn04 <shim1376@naver.com>
2025-03-14 14:34:45 +0900

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello!! My First Java Coding!!");
+        System.out.print("My Name Is Sangho");
+        System.out.print("Hello Everyone");}
+ }
+
+
