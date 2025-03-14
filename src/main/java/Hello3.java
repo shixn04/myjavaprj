369f5dbcb25f6e6067f09b7c7ca040bbe78aacd9
shixn04 <shim1376@naver.com>
2025-03-14 15:29:23 +0900

    4번째

--- src/main/java/Hello3.java
+++ src/main/java/Hello3.java
@@ -1,6 +1,6 @@
 public class Hello3 {
     public static void main(String[] args) {
-        System.out.println("Hello3 My Second Java Cooding!");
+        System.out.println("Hello3 My Second Java Cooing!");
 
         System.out.println("Edit Java code!");
 
