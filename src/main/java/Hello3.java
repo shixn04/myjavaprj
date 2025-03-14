d4fdafe059b84b3c47dae8d64c60140ead4fba0c
shixn04 <shim1376@naver.com>
2025-03-14 14:57:16 +0900

    3번째

---
+++ src/main/java/Hello3.java
@@ -0,0 +1,8 @@
+public class Hello3 {
+    public static void main(String[] args) {
+        System.out.println("Hello3 My Second Java Cooding!");
+
+        System.out.println("Edit Java code!");
+
+    }
+}
