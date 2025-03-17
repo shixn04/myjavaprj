9ccf0375c9333292585548a0786789edb948fb48
shixn04 <shim1376@naver.com>
2025-03-17 10:31:55 +0900

    4번째

---
+++ src/main/java/Java1000.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Java1000 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt(100);
+        b = sc.nextInt(200);
+        System.out.println(a+b);
+
+    }
+}
