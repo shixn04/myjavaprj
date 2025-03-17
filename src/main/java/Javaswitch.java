c0a59aa5043f7cdef7285f0baa6942dc685c3c83
shixn04 <shim1376@naver.com>
2025-03-17 16:32:59 +0900

    7번째

---
+++ src/main/java/Javaswitch.java
@@ -0,0 +1,12 @@
+public class Javaswitch{
+
+        public static void main(String[] args) {
+            int result=1;
+            result *= 5;
+            result++;
+            result%=2;
+            result++;
+
+            System.out.println(result);
+        }
+}
