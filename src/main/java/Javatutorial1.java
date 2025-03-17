58ccb362842ce7b6fa9199f4f6a14864ea822e8b
shixn04 <shim1376@naver.com>
2025-03-17 16:42:57 +0900

    8번째

---
+++ src/main/java/Javatutorial1.java
@@ -0,0 +1,24 @@
+public class Javatutorial1 {
+        public static void main(String[] args) {
+                String var1 = "tutorial";
+                        String var2 = "tutorial";
+
+                        byte var3 = -128;
+                        short var4 = -32768;
+                        int var5 = 999999;
+                         long var6 = 9999999;
+
+                         float var7 = 0.5f;
+                         double var8 = 0.00005;
+
+            System.out.println(var1);
+                System.out.println(var2);
+                    System.out.println(var3);
+            System.out.println(var4);
+            System.out.println(var5);
+            System.out.println(var6);
+            System.out.println(var7);
+            System.out.println(var8);
+            
+        }
+}
