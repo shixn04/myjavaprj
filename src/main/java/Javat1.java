ccb10bd7ff0254baf5d94a79690ebbc800b253da
shixn04 <shim1376@naver.com>
2025-03-18 13:21:41 +0900

    오류투성이

---
+++ src/main/java/Javat1.java
@@ -0,0 +1,8 @@
+public class Javat1 {
+   public static void main(String[] args) {
+       final double PI = 3.14159;
+       int radius = 5;
+       double result = radius * 2 * PI ;
+       System.out.println(result);
+    }
+}
