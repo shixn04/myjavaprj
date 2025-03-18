858eeca46f889ed156815e533349903ef05c0856
shixn04 <shim1376@naver.com>
2025-03-18 13:32:39 +0900

    오류투성이

---
+++ src/main/java/Javat3.java
@@ -0,0 +1,17 @@
+public class Javat3 {
+
+    public static void main(String[] args) {
+            boolean isFun = false;
+                System.out.println(isFun);
+                    char c = 'f';
+                        int x = 59;
+                            long bing = 345455433L;
+                            float f =32.4f;
+                            double d =3434343.5;
+                          System.out.println(x);
+                                  System.out.println(c);
+                                          System.out.println(bing);
+            System.out.println(f);
+                System.out.println(d);
+    }
+}
