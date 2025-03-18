b11e46b1cc67b353444f30bf5f234b221747dec2
shixn04 <shim1376@naver.com>
2025-03-18 13:24:38 +0900

    오류투성이

---
+++ src/main/java/Javat2.java
@@ -0,0 +1,10 @@
+public class Javat2 {
+        public static void main(String[] args) {
+                int radius = 5;
+                    final double PI = 3.141592;
+
+                        double result = radius * 2 * PI;
+                            System.out.println(result);
+
+        }
+}
