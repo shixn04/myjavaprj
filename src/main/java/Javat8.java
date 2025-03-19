7a2072af0c45844d7c49eccae1c0e43a22462ec1
shixn04 <shim1376@naver.com>
2025-03-19 12:13:46 +0900

    오류투성이

---
+++ src/main/java/Javat8.java
@@ -0,0 +1,7 @@
+public class Javat8 {
+    public static void main(String[] args) {
+        long longValue = 20;
+        int intValue = (int)longValue;
+        System.out.println(intValue);
+    }
+}
