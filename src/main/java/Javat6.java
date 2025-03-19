6e9a2d630323475e57262939f9b43e4687d2cc04
shixn04 <shim1376@naver.com>
2025-03-19 12:05:52 +0900

    오류투성이

---
+++ src/main/java/Javat6.java
@@ -0,0 +1,13 @@
+public class Javat6 {
+    public static void main(String[] args){
+        int x = 50000;
+        long y = 50000 ;
+
+        long x2 = 5;
+        int y2 =(int) x2;
+        System.out.println(y2);
+        System.out.println(x2);
+        System.out.println(y);
+        System.out.println(x);
+    }
+}
