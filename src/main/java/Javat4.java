749c23b9c628812a38b137780101ef3f85b6dc44
shixn04 <shim1376@naver.com>
2025-03-19 11:02:10 +0900

    오류투성이

---
+++ src/main/java/Javat4.java
@@ -0,0 +1,17 @@
+public class Javat4 {
+    public static void main (String[] args) {
+        boolean isFun = false;
+        System.out.println(isFun);
+        char c= 'f';
+                int x = 59;
+        long bing =3453455433L;
+        float f = 32.4f;
+        double d =3434343.4;
+        System.out.println(x);
+        System.out.println(bing);
+            System.out.println(f);
+            System.out.println(d);
+            System.out.println(c);
+
+    }
+}
