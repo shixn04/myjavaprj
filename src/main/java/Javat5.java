cddee9629ecafc59c2be1c41a787835aab93dbb9
shixn04 <shim1376@naver.com>
2025-03-19 11:29:00 +0900

    오류투성이

---
+++ src/main/java/Javat5.java
@@ -0,0 +1,15 @@
+public class Javat5 {
+    public static void main(String[] args) {
+        boolean isFun = false;
+        char charValue = 'a';
+        int intValue = 20;
+        long LongValue = 2147483648L;
+
+        System.out.println(LongValue);
+        System.out.println(charValue);
+        System.out.println(intValue);
+        System.out.println(LongValue);
+        System.out.println(isFun);
+    }
+
+}
