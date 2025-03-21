edd5325fd17a4720b9ebf23850d7700c311df7fa
shixn04 <shim1376@naver.com>
2025-03-21 13:14:59 +0900

    시험준비

--- src/main/java/Javatutorial3.java
+++ src/main/java/Javatutorial3.java
@@ -1,21 +1,6 @@
-import java.awt.*;
+public class Javatutorial3 {
+    public static void main(String[] args) {
 
-class Javatt3{
-        int x;
-        int y;
-}
-public class javatt3 {
-        public static void main (String[] args) {
-            Point m = new Point();
-            m.x=1000;
-            calc(m);
-            getValue(m);
-            System.out.printf("%d",m.y);
-        }
-        static void clac(Point s){
-            s.x /= 10;
-        }
-        static void getValue(Point p){
-            p.y = p.x;
-        }
+        System.out.println("Hello World");
+    }
 }
