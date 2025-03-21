4997a6c6799f30243f339a9b2527a1ee9f6a0bc5
shixn04 <shim1376@naver.com>
2025-03-21 13:16:43 +0900

    시험준비

---
+++ src/main/java/Javay14.java
@@ -0,0 +1,9 @@
+public class Javay14 {
+    public static void main(String[] args) {
+        int a= 100;
+        int b= 200;
+
+        int c = a + b;
+        System.out.println(a+ "+" + b + "=" + c);
+    }
+}
