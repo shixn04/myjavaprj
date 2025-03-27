a0b0fb510d58e42ff7c9f340fcba838791bf7de4
shixn04 <shim1376@naver.com>
2025-03-27 21:23:40 +0900

    시험준비

---
+++ src/main/java/Javat15.java
@@ -0,0 +1,19 @@
+public class Javat15{
+
+    public static void main(String[] args) {
+
+        int num1, num2, result;
+        num1 = 100;
+        num2 = 60;
+
+        float res, res2;
+
+        result = num1/ num2;
+        res = (float) num1 / num2;
+        res2 = (float) num1 /num2;
+
+        System.out.println(num1 + "/" + num2 + "=" + result);
+        System.out.println(num1 + "/" + num2 + "=" + res);
+        System.out.println(num1 + "/" + num2 + "=" + res2);
+    }
+}
