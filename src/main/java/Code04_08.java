b93a243191e923c3a51945a5dd5e4b7cf839bbfe
shixn04 <shim1376@naver.com>
2025-03-28 15:22:25 +0900

    시험준비

---
+++ src/main/java/Code04_08.java
@@ -0,0 +1,9 @@
+public class Code04_08 {
+    public static void main(String[] args){
+        String var1 = """
+                난생처음\s
+                자바를\s
+                열공 주입니다.""";
+        System.out.println(var1);
+    }
+}
