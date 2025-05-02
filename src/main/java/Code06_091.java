1a9b9f6696de68dd4a3091e41158907fa8c1e569
shixn04 <shim1376@naver.com>
2025-05-02 14:47:16 +0900

    잘올려보자

---
+++ src/main/java/Code06_091.java
@@ -0,0 +1,13 @@
+public class Code06_091 {
+    public static void main(String[] args) {
+        for (int i = 2; i <10 ; i++) {
+
+            for (int j = 1; j< 10; j++) {
+                int result = j * i;
+                System.out.print(i + "x" + j + "=" + result);
+            }
+                System.out.println();
+
+        }
+    }
+}
